bdb366f5b6f4d37f829ee4ed254b676fabf8d414
bhavishyachandra <bhavishyachandra@gmail.com>
2026-05-19 19:48:04 +0000

    Address Copilot PR comments on tag redirect and icon URLs

--- src/FaceIT.Hangfire.Tags/Resources/script.js
+++ src/FaceIT.Hangfire.Tags/Resources/script.js
@@ -25,7 +25,11 @@ $(function() {
     $(".tags a").tagcloud();
     $("#btn_tags_go").click(function () {
         var baseUrl = $(this).data("base-url");
-        var tag = $("#selectedTag").val();
-        window.location = baseUrl + tag;
+        var tag = ($("#selectedTag").val() || "").trim();
+        if (!tag) {
+            return;
+        }
+
+        window.location = baseUrl + encodeURIComponent(tag);
     });
 });
